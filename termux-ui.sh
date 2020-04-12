1e72ae8232aa23899537ca839f3bfe8eb30ce26a
abhackerofficial <63346676+abhackerofficial@users.noreply.github.com>
2020-04-12 20:31:27 +0530

    Update termux-ui.sh

--- termux-ui.sh
+++ termux-ui.sh
@@ -1,8 +1,6 @@
 echo -e "\033[1;92m"[*] This Script is written by ÃBHàçkēr Øffïçïāl
 echo -e "\033[1;36;40m"[*] Installation Has Started...
 echo -e "\033[1;91m"[*] Please wait...
-apt update -y
-apt upgrade -y
 apt install nano -y
 apt install bash -y
 apt install python -y
