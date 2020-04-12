f31fe47ae99be281a226378d8bc2b6b8ed809ef4
abhackerofficial <63346676+abhackerofficial@users.noreply.github.com>
2020-04-12 19:56:25 +0530

    Add files via upload

---
+++ termux-ui.sh
@@ -0,0 +1,18 @@
+echo -e "\033[1;92m"[*] This Script is written by ÃBHàçkēr Øffïçïāl
+echo -e "\033[1;36;40m"[*] Installation Has Started...
+echo -e "\033[1;91m"[*] Please wait...
+apt update -y
+apt upgrade -y
+apt install git -y
+apt install nano -y
+apt install bash -y
+apt install python -y
+apt install python2 -y
+apt install ruby -y
+gem install lolcat
+apt install cowsay -y
+apt install figlet -y
+apt install toilet -y
+echo -e "\033[1;33m"[*] Installing Termux-ohmyzsh
+echo -e "\033[1;34m"[*] Please wait...
+sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
