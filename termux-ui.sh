71498c7ebe91c3951596210fd5e077e9a91f61d0
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-06-05 15:35:03 +0530

    Update termux-ui.sh

--- termux-ui.sh
+++ termux-ui.sh
@@ -12,12 +12,23 @@ echo -e '\033[38;2;0;255;150m
 \033[0m'
 printf "     \e[102m\e[1;90m >> youtube.com/\e[104m\e[1;77mabhackerofficial << \e[0m\n"
 sleep 2
-echo -e "\033[1;92m"[*] This Script is written by ABHacker Official
+echo
+echo -e "\033[1;92m[*] This Script is written by ABHacker Official"
 sleep 1
-echo -e "\033[1;36;40m"[*] Installation Has Started...
-sleep 1
-echo -e "\033[1;91m"[*] Please wait...
+echo
+echo -e "\033[1;96m[*] Installation Has Started..."
 
+connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
+if [[ "$connection" != 0 ]]
+then clear
+echo -e "\033[1;91m [\033[1;92m-\033[1;91m] No Internet\033[1;92m connection!"
+   exit
+fi
+sleep 1
+echo
+echo -e "\033[1;91m[*] Please wait..."
+echo
+echo -e "\033[1;91m[+] Installing Required Packages..."
 command -v nano > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require nano but it's not installed, Now Installing.\e[1;92m"; apt install nano -y; echo; }
 command -v python > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require python but it's not installed, Now Installing.\e[1;92m"; apt install python -y; echo; }
 command -v python2 > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require python2 but it's not installed, Now Installing.\e[1;92m"; apt install python2 -y; echo; }
@@ -27,8 +38,12 @@ command -v cowsay > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require cowsay 
 command -v lolcat > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require lolcat but it's not installed, Now Installing.\e[1;92m"; gem install lolcat; echo; }
 command -v toilet > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require toilet but it's not installed, Now Installing.\e[1;92m"; apt install toilet -y; echo; }
 
-echo -e "\033[1;33m"[*] Installing Termux-ohmyzsh
-echo -e "\033[1;34m"[*] Please wait...
+echo -e "\033[1;93m[*] Installing Termux-ohmyzsh"
+sleep 1
+echo
+echo -e "\033[1;94m[*] Please wait..."
+sleep 1
+echo
 sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
 rm -rf termux-ohmyzsh > /dev/null 2>&1
 kill -9 $PPID
