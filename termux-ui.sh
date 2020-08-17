a76cbf0c183cec179d8f287f9b1f0edca5388cb2
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-08-18 03:31:46 +0530

    Update termux-ui.sh

--- termux-ui.sh
+++ termux-ui.sh
@@ -13,10 +13,11 @@ echo -e '\033[38;2;0;255;150m
 printf "     \e[102m\e[1;90m >> youtube.com/\e[104m\e[1;77mabhackerofficial << \e[0m\n"
 sleep 2
 echo
-echo -e "\033[1;92m[*] This Script is written by ABHacker Official"
+command -v pv > /dev/null 2>&1 || apt install pv -y &> /dev/null
+echo -e "\033[1;38;2;0;255;100m[$] Coding by ABHacker Official..." | pv -qL 20
 sleep 1
 echo
-echo -e "\033[1;96m[*] Installation Has Started..."
+echo -e "\033[1;96m[*] Installation Has Started..." | pv -qL 20
 
 connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
 if [[ "$connection" != 0 ]]
@@ -26,9 +27,9 @@ echo -e "\033[1;91m [\033[1;92m-\033[1;91m] No Internet\033[1;92m connection!"
 fi
 sleep 1
 echo
-echo -e "\033[1;91m[*] Please wait..."
+echo -e "\033[1;91m[*] Please wait..." | pv -qL 20
 echo
-echo -e "\033[1;91m[+] Installing Required Packages..."
+echo -e "\033[1;91m[+] Installing Required Packages..." | pv -qL 20 
 command -v nano > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require nano but it's not installed, Now Installing.\e[1;92m"; apt install nano -y; echo; }
 command -v python > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require python but it's not installed, Now Installing.\e[1;92m"; apt install python -y; echo; }
 command -v python2 > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require python2 but it's not installed, Now Installing.\e[1;92m"; apt install python2 -y; echo; }
@@ -39,12 +40,14 @@ command -v lolcat > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require lolcat 
 command -v toilet > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require toilet but it's not installed, Now Installing.\e[1;92m"; apt install toilet -y; echo; }
 echo
 
-echo -e "\033[1;93m[*] Installing Termux-ohmyzsh"
+echo -e "\033[1;93m[*] Installing Termux-Ohmyzsh " | pv -qL 20
 sleep 1
 echo
-echo -e "\033[1;94m[*] Please wait..."
+if [ -d "/data/data/com.termux/files/home/.oh-my-zsh" ];then echo -e "\e[1;38;2;0;255;200m[√] OH-MY-ZSH Already Installed !\n" ; fi
+if [ ! -d "/data/data/com.termux/files/home/.oh-my-zsh" ];then
+echo -e "\033[1;94m[*] Please wait..." | pv -qL 20
 sleep 1
 echo
-sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
-rm -rf termux-ohmyzsh > /dev/null 2>&1
+sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)" ; fi
+if [ -d "/data/data/com.termux/files/home/.oh-my-zsh" ];then $HOME ; rm -rf termux-ohmyzsh ; fi
 kill -9 $PPID
